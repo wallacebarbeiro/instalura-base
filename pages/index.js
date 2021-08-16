39c6358a0399359794ba47071e53974d61cc770f
wallace <wallace.barbeiro@gmail.com>
2021-08-16 12:51:46 -0300

    ci(husky): config

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react';  
+import React from 'react';
 import Button from '../src/components/commons/Button';
 import Footer from '../src/components/commons/Footer';
 import Menu from '../src/components/commons/Menu';
