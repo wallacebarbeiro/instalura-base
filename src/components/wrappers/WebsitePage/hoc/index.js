c8c70bf028aa075351a4b838a1d6509806fb1a70
wallace <wallace.barbeiro@gmail.com>
2021-08-26 20:46:19 -0300

    feat: final aula 05

--- src/components/wrappers/WebsitePage/hoc/index.js
+++ src/components/wrappers/WebsitePage/hoc/index.js
@@ -1,13 +1,20 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
 import React from 'react';
 import WebsitePageWrapper from '..';
 import WebsiteGlobalProvider from '../provider';
 
-export default function websitePageHOC(Component, { pageWrapperProps }) {
+export default function websitePageHOC(
+  Component,
+  { pageWrapperProps } = { pageWrapperProps: {} },
+) {
   return (props) => (
     <WebsiteGlobalProvider>
       <WebsitePageWrapper
         // eslint-disable-next-line react/jsx-props-no-spreading
         {...pageWrapperProps}
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        {...props.pageWrapperProps}
       >
         <Component
           // eslint-disable-next-line react/jsx-props-no-spreading
