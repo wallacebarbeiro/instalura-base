09267f0568798d879911cdf5cc9f6b3c3d0a77cc
wallace <wallace.barbeiro@gmail.com>
2021-09-21 19:40:56 -0300

    feat: final m05a03

---
+++ src/components/screens/AboutScreen/getContent.js
@@ -0,0 +1,19 @@
+/* eslint-disable import/prefer-default-export */
+import { CMSGraphQLClient, gql } from '../../../infra/cms/CMSGraphQLClient';
+
+export async function getContent() {
+  const query = gql`
+    query {
+      pageSobre {
+        pageTitle
+        pageDescription
+      }
+    }
+  `;
+
+  const client = CMSGraphQLClient();
+
+  const response = await client.query({ query });
+
+  return response.data.messages;
+}
