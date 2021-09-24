d960ba8bf2745958b6a16ed8bf1dcc75b0deb6e6
wallace <wallace.barbeiro@gmail.com>
2021-09-23 21:47:08 -0300

    feat: acerto testeLogin

--- src/services/login/loginService.test.js
+++ src/services/login/loginService.test.js
@@ -29,7 +29,7 @@ describe('loginService', () => {
           }, setCookieModule, HttpClientModule);
 
           expect(setCookieModule).toHaveBeenCalledWith(
-            null, 'APP_TOKEN', token, {
+            null, 'LOGIN_COOKIE_APP_TOKEN', token, {
               path: '/',
               maxAge: 604800,
             },
@@ -56,7 +56,7 @@ describe('loginService', () => {
       test('remove its token', async () => {
         const destroyCookie = jest.fn();
         await loginService.logout(null, destroyCookie);
-        expect(destroyCookie).toHaveBeenCalledWith(null, 'APP_TOKEN'); // que apague o token
+        expect(destroyCookie).toHaveBeenCalledWith(null, 'LOGIN_COOKIE_APP_TOKEN', { path: '/' }); // que apague o token
       });
     });
   });
